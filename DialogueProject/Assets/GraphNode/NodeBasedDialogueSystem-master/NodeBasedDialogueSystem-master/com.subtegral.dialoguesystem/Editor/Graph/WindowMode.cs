0d2af21632206de32f6cc57199c7872f82dcf354
Stevens Tavares-Ramos <stevens.tavares@gmail.com>
2026-01-14 11:19:28 +0100

    [V_0.1.1] Mise en place archi Window Mode

---
+++ DialogueProject/Assets/GraphNode/NodeBasedDialogueSystem-master/NodeBasedDialogueSystem-master/com.subtegral.dialoguesystem/Editor/Graph/WindowMode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.GraphNode.NodeBasedDialogueSystem_master.NodeBasedDialogueSystem_master.com.subtegral.dialoguesystem.Editor.Graph
+{
+
+    public enum Mode 
+    {
+        Panel,
+        Bubble,
+        Popup
+    }
+
+    internal class WindowMode : ScriptableObject
+    {
+        public Mode mode;
+
+        public void SwitchWindowMode(Mode mode)
+        {
+
+            switch (mode) 
+            {
+                case Mode.Panel:
+                    Panel();
+                    break;
+
+                case Mode.Popup:
+                    Popup();
+                    break;
+
+                case Mode.Bubble:
+                     Bubble();
+                    break;
+
+                default: Console.WriteLine("No mode find please fix it");
+                        break;
+            }
+            
+        }
+
+        public void Panel() 
+        {
+            //TODO
+        }
+
+        public void Bubble()
+        {
+            //TODO
+        }
+
+        public void Popup() 
+        {
+            //TODO
+        }
+    }
+}
